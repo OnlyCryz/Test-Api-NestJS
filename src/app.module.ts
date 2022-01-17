b091ba15d7eec4ad91e4ad2f5c4fab6f136abfab
OnlyCryz <cristobal.c9707@gmail.com>
2022-01-17 01:08:02 -0300

    Correcciones de los modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StudentsController } from './students/controllers/students.controller';
-import { StudentsService } from './students/services/students.service';
+import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, StudentsController],
-  providers: [AppService, StudentsService],
+  imports: [StudentsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
