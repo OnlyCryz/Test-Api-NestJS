487d7f68b72764b7afe90e84b7306ced5ff302ac
OnlyCryz <cristobal.c9707@gmail.com>
2022-01-17 01:51:41 -0300

    Nuevo modulo agregado Universidad

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
+import { UniversitiesModule } from './universities/universities.module';
 
 @Module({
-  imports: [StudentsModule],
+  imports: [StudentsModule, UniversitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
